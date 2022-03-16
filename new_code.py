1e96d23bbbd1769092a77c763a49690e73d5a3d4
vetal <cheka13vet@mail.ru>
2022-03-16 22:44:50 +0300

    test_22:44

--- new_code.py
+++ new_code.py
@@ -1,19 +1,35 @@
-err = {
-        1: 'пароль слишком короткий',
-        2: 'пароль слишком длинный',
-        3: 'в пароле отсутсвуют цифры',
-        4: 'в пароле отсутсвуют строчные буквы',
-        5: 'в пароле отсутсвуют прописные буквы',
-    }
-def check_pass(pswd):
-    a=len(pswd)
-    if a < 8:
-        print (f'{err[1]}')
-    elif a > 8:
-        print (f'{err[2]}')
-    else:
-        print  (f'{err[3]}')   
-    return print (a)
-print (check_pass('1234567'))
-print (check_pass('12345678'))
-print (check_pass('123456789'))
+import numpy as np
+def random_predict(number:int=1) -> int:
+    random_number = np.random.randint(1, 101) # загадываем число
+    count = 0
+    min = 1
+    max = 100
+    mid=0
+    while True:
+        count+=1
+        mid = round((min+max) / 2) #берём середину диапазона
+        
+        if mid > random_number:
+            max = mid
+        elif mid < random_number:
+            min = mid
+        else:
+            
+            break #конец игры выход из цикла    
+    return(count)
+
+def score_game(random_predict) -> int:
+    count_ls = []
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+score_game(random_predict)
